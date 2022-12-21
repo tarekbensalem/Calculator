4898772cd3b4a1a3c4e3fbb8be76d3d0943bfae0
Tarek <taryk.ben.salum@gmail.com0>
2022-12-21 16:41:08 +0100

    it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import './index.css'
+import { useState } from 'react'
 function App() {
+
+  const i = () =>{
+    const table =[]
+
+    for (let i=1;i<10;i++){
+        table.push(
+            <button key={i}  onClick={()=> update(i)} >{i}</button>
+        )
+    } return table
+  }
+   
+  const [calc,setCalc] = useState("");
+  
+  const update = (value) =>{
+    
+    setCalc(calc+value);
+  }
+  
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="calculator">
+          <div className="display" >
+            {calc || 0} 
+         </div>
+         <div className='operators'>
+      <button onClick={()=> update('/')}>/</button>
+      <button onClick={()=> update('*')}>*</button>
+    <button onClick={()=> update('+')}>+</button>
+    <button onClick={()=> update('-')}>-</button>
+    <button onClick={()=> setCalc("")} >delete</button>
+    </div>
+    <div className='number' >
+      {i() }
+      <button onClick={()=> update('0')}>0</button>
+      <button onClick={()=> update('.')}>.</button>
+      <button onClick={()=>{}}>=</button>
+      
+    </div>
+
+    </div>
     </div>
   );
 }
